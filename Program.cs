3d689d96ca1417018f68ac8617de8f2abf85f526
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-03 11:05:47 -0300

    Adiciona resolução ao exercício Par Impar ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num, resto;
+
+            Console.Write("Olá usuário! Por favor digite um número: ");
+
+            num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            resto = num % 2;
+
+            /*Console.WriteLine(resto);
+            Console.WriteLine();*/
+
+            if(resto == 0)
+            {
+                Console.Write($"O número {num} é: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("par");
+                Console.ResetColor();
+                Console.WriteLine(".");
+            }
+            else
+            {
+                Console.Write($"O número {num} é: ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("impar");
+                Console.ResetColor();
+                Console.WriteLine(".");             
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ParImpar
-Exercício Par Impar ETEC 2021
+## Exercício `ParImpar` ETEC 2021
+
+Receba um número. Exiba "par" ou "ímpar", conforme o caso.
+
+_Dica: um número é ímpar caso possua resto ao ser dividido por 2._
+
+Ex.:
+
+```
+Digite um número: 17
+17 é ímpar
+```
